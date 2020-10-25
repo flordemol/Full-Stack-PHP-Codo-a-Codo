58bc6563b2d17d33f2030c7f538aac142bda4fd0
Florencia <florenciademollein@gmail.com>
2020-10-25 18:44:40 -0300

    Filtros en index y ejercicios de validaciones JS

---
+++ js/main.js
@@ -0,0 +1,291 @@
+const tareas = [
+    {
+        "id" : 23,
+        "href" : "desafioJsBoletos/alumnos.html",
+        "imagen" : "desafioJsBoletos.JPG",
+        "altIMG" : "Imagen del desafío Manejo del DOM con JavaScript",
+        "titulo" : "Desafío JavaScript (Boletos) - Manejo del DOM",
+        "herramientas" : ["Javascript"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 22,
+        "href" : "JS_Unidad3_Validaciones/unidad8.html",
+        "imagen" : "validaciones.JPG",
+        "altIMG" : "Imagen del ejercicio Validaciones con JavaScript",
+        "titulo" : "Validaciones - Act. obligatoria JavaScript Unidad 3",
+        "herramientas" : ["HTML", "CSS", "Javascript"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 21,
+        "href" : "JS_ejercicios_adicionales/Ejercicios_adicionales_de_Javascript_1.html",
+        "imagen" : "ejercicios_adicionales_de_javascript_1.JPG",
+        "altIMG" : "Imagen de los ejercicios adicionales 1 de JavaScript",
+        "titulo" : "Ejercicios adicionales de JavaScript 1",
+        "herramientas" : ["HTML", "Javascript"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 20,
+        "href" : "Proyecto web 'Conf Bs As'/index.html",
+        "imagen" : "confBsAs.JPG",
+        "altIMG" : "Imagen del proyecto Conf Bs As",
+        "titulo" : "PROYECTO WEB 'CONF BS AS' (Bootstrap)",
+        "herramientas" : ["HTML", "CSS", "Bootstrap"],
+        "tipoActividad" : "Proyecto"
+    },
+    {
+        "id" : 19,
+        "href" : "act_obligat_js_TABLAS_u2.html",
+        "imagen" : "tablas.JPG",
+        "altIMG" : "Imagen del ejercicio Formulario Bootstrap",
+        "titulo" : "Tablas de multiplicar - Act. obligatoria JavaScript Unidad 2",
+        "herramientas" : ["HTML", "Javascript"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 18,
+        "href" : "act_obligat_js_u1.html",
+        "imagen" : "unidad1_JS.JPG",
+        "altIMG" : "Imagen del ejercicio Formulario Bootstrap",
+        "titulo" : "Sumar y escribir por pantalla - Act. obligatoria JavaScript Unidad 1",
+        "herramientas" : ["Javascript"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 17,
+        "href" : "act_obligatoria_Form_boostrap_unidad2.html",
+        "imagen" : "form.JPG",
+        "altIMG" : "Imagen del ejercicio Formulario Bootstrap",
+        "titulo" : "Formulario - Act. obligatoria Bootstrap Unidad 2",
+        "herramientas" : ["HTML", "CSS", "Bootstrap"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 16,
+        "href" : "act_obligatoria_Cards_boostrap_unidad1.html",
+        "imagen" : "cards.JPG",
+        "altIMG" : "Imagen del ejercicio Cards",
+        "titulo" : "Cards - Act. obligatoria Bootstrap Unidad 1",
+        "herramientas" : ["HTML", "Bootstrap"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 15,
+        "href" : "desafioFontAwesome.html",
+        "imagen" : "fontAwesome.JPG",
+        "altIMG" : "Imagen del ejercicio FontAwesome",
+        "titulo" : "Desafío Font Awesome",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 14,
+        "href" : "Desafío_Google/DesafioGoogle_17-9.html",
+        "imagen" : "google.JPG",
+        "altIMG" : "Imagen del ejercicio Google",
+        "titulo" : "Actividad Desafío 17/9 (Clon Google)",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 13,
+        "href" : "actividad_10-9_position_flexbox.html",
+        "imagen" : "position_flexbox.JPG",
+        "altIMG" : "Imagen del ejercicio Position/Flexbox",
+        "titulo" : "Actividad Desafío 10/9 (position/flexbox)",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 12,
+        "href" : "actividad_obligatoria_unidad_2_css.html",
+        "imagen" : "cajasCSS.JPG",
+        "altIMG" : "Imagen del ejercicio Cajas CSS",
+        "titulo" : "Actividad Obligatoria Unidad 2 CSS (cajas)",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 11,
+        "href" : "facebook.html",
+        "imagen" : "facebook.JPG",
+        "altIMG" : "Imagen del ejercicio Recrear página",
+        "titulo" : "Actividad 3/9 - Recrear página (facebook)",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Desafio"
+    },
+    {
+        "id" : 10,
+        "href" : "Proyecto_Bienes_Raices/index.html",
+        "imagen" : "bienesRaices.JPG",
+        "altIMG" : "Imagen del ejercicio de Bienes Raíces",
+        "titulo" : "PROYECTO WEB 'BIENES RAÍCES' (HTML y CSS)",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Proyecto"
+    },
+    {
+        "id" : 9,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=3557",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Actividad Martes 1/9 - Subir archivo FTP (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 8,
+        "href" : "act_unidad2_form.html",
+        "imagen" : "miniformulario.JPG",
+        "altIMG" : "Imagen del ejercicio Miniformulario",
+        "titulo" : "Mini formulario - Actividad obligatoria HTML Unidad 2",
+        "herramientas" : ["HTML"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 7,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=2867",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Actividad jueves 27/8 (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 6,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=1901",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Nombre de Subdominio (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 5,
+        "href" : "act2_tabla.html",
+        "imagen" : "tablaConIframe.JPG",
+        "altIMG" : "Imagen del ejercicio Tabla con Iframe",
+        "titulo" : "Tabla con iframe - Actividad de investigación HTML Unidad 2",
+        "herramientas" : ["HTML", "CSS"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 4,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=1313",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Actividad 13/8 - Estructura HTML (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Desafío"
+    },
+    {
+        "id" : 3,
+        "href" : "receta.html",
+        "imagen" : "receta.JPG",
+        "altIMG" : "Imagen del ejercicio Receta",
+        "titulo" : "Ejercicio Obligatorio Unidad 1 HTML (Receta)",
+        "herramientas" : ["HTML"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 2,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=682",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Actividad Investigacion 1 (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Actividad obligatoria"
+    },
+    {
+        "id" : 1,
+        "href" : "https://aulasvirtuales.bue.edu.ar/mod/forum/discuss.php?d=625",
+        "imagen" : "logoCodoACodo.png",
+        "altIMG" : "Logo de Programa Codo a Codo",
+        "titulo" : "Presentacion Personal (Foro)",
+        "herramientas" : ["Foro"],
+        "tipoActividad" : "Actividad obligatoria"
+    }
+]
+
+const renderActividades = document.getElementById("listaActividades");
+const renderTipoAct = document.getElementById("tipoAct");
+const renderHerramientas = document.getElementById("herramienta");
+
+// Mostrar icono en la tarjeta
+function iconos(tarea) {
+    let iconos = "";
+
+    if(tarea.herramientas.includes('Foro')) 
+        iconos += '<i class="fas fa-chalkboard-teacher"></i>'
+    if(tarea.herramientas.includes('HTML')) 
+        iconos += '<i class="fab fa-html5"></i>'
+    if(tarea.herramientas.includes('CSS')) 
+        iconos += '<i class="fab fa-css3-alt"></i>'
+    if(tarea.herramientas.includes('Bootstrap')) 
+        iconos += '<i class="fab fa-bootstrap"></i>'
+    if(tarea.herramientas.includes('Javascript')) 
+        iconos += '<i class="fab fa-js-square"></i>'
+    
+    return iconos;
+}
+
+// Borra todas las tarjetas
+function limpiarActividades(){
+    renderActividades.innerHTML = "";
+}
+
+// Filtros
+renderTipoAct.addEventListener("change", chequearFiltros);
+renderHerramientas.addEventListener("change", chequearFiltros);
+
+function chequearFiltros(){
+    const tipoAct = renderTipoAct.options[renderTipoAct.selectedIndex].text;
+    const herr = renderHerramientas.options[renderHerramientas.selectedIndex].text;
+
+    listarActividades(tipoAct, herr)
+}
+
+function listarActividades(tipoAct, herr){
+    console.log(tipoAct + " " + herr)
+    limpiarActividades();
+    
+    for(let tarea of tareas){
+        
+        if(tipoAct == "Todo" && herr == "Todo") render(tarea);
+        
+        if((tarea.tipoActividad == tipoAct) && (tarea.herramientas.includes(herr))) render(tarea);
+
+        if(tipoAct == "Todo" && tarea.herramientas.includes(herr)) render(tarea);
+        
+        if((tarea.tipoActividad == tipoAct) && herr == "Todo") render(tarea);
+    }
+}
+
+listarActividades("Todo", "Todo");
+
+// Crear tarjeta
+function render(tarea){
+    let ico = iconos(tarea);
+
+    renderActividades.innerHTML += `
+    <div class="col-lg-4 col-md-6 col-12">
+          <a href="${tarea.href}" target="_blank">
+            <div class="card">
+              <img
+                src="imagenes/${tarea.imagen}"
+                class="card-img-top"
+                alt="${tarea.altIMG}"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  ${tarea.titulo} 
+                </p>
+                <p class="card-text logos">
+                  ${ico}
+                </p>
+              </div></div
+          ></a>
+        </div>
+    `
+}
